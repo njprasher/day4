c7840abcee9d9447b5fd3df378ca2af91456cd59
Neeraj Prasher <prasher.neeraj99@gmail.com>
2019-05-27 21:33:40 -0400

    [Neeraj] Constructor, Setter/Getter, Method Declaration, Array(Marks)and Got a dummy object and showed its details.

--- src/main/java/com/neeraj/StudentMain.java
+++ src/main/java/com/neeraj/StudentMain.java
@@ -48,6 +48,10 @@ public class StudentMain
         return lastName;
     }
 
+    public String getFullname() {
+        return this.firstName + " " + this.lastName;
+    }
+
     public void setLastName(String lastName) {
         this.lastName = lastName;
     }
@@ -88,22 +92,113 @@ public class StudentMain
         return result;
     }
 
-    void calculateTotal()
+    public void calculateTotal()
     {
         this.totalMarks = 0.0f;
-        for (int i = 0; i<marks.length; i++)
+        for (int i = 0; i<this.marks.length; i++)
         {
             this.totalMarks = this.totalMarks + marks[i];
         }
     }
 
-    void calculatePercentage()
+    public void calculatePercentage()
     {
-
+        this.percentage = 0.0f;
+        this.percentage=this.totalMarks/this.marks.length;
     }
 
-    void calculateResult()
+    public void calculateResult()
     {
+        int passedSubjects = 0;
+        for(int i = 0; i < marks.length; i++)
+        {
+            if (this.percentage>50.0f)
+            {
+                passedSubjects++;
+            }
+        }
+
+        if (passedSubjects<=2) this.result = "Fail";
+
+        else{
+
+            if (this.percentage>=94.0f)
+            {
+                this.result = "A+";
+            }
+            else if (this.percentage>=87.0f)
+            {
+                this.result = "A";
+            }
+            else if (this.percentage>=80.0f)
+            {
+                this.result = "A-";
+            }
+            else if (this.percentage>=77.0f)
+            {
+                this.result = "B+";
+            }
+            else if (this.percentage>=73.0f)
+            {
+                this.result = "B";
+            }
+            else if (this.percentage>=70.0f)
+            {
+                this.result = "B-";
+            }
+            else if (this.percentage>=67.0f)
+            {
+                this.result = "C+";
+            }
+            else if (this.percentage>=63.0f)
+            {
+                this.result = "C";
+            }
+            else if (this.percentage>=60.0f)
+            {
+                this.result = "C-";
+            }
+            else if (this.percentage>=50.0f)
+            {
+                this.result = "D";
+            }
+            else this.result = "F";
+        }
+    }
+
+    public void displayStudentInformation()
+    {
+
+
+
+        this.setStudentId(1);
+        //s1.setBirthDate(date.toString());
+        this.setFirstName("Neeraj");
+        this.setLastName("Prasher");
+        this.setGender("Male");
+
+
+        float[] marksToShow = this.getMarks();
+
+        System.out.println(
+                        "---------------------------------- \n" +
+                        "      ------STUDENT INFO------      \n" +
+                        "---------------------------------- \n" +
+                        "Student ID : " + this.getStudentId() + "\n" +
+                        "Full Name : " + this.getFullname() + "\n" +
+                        "Gender : " + this.getGender() + "\n" +
+                        "Birth Date : " + this.getBirthDate() + "\n" );
+
+        for(int i = 0; i < this.marks.length; i++)
+        {
+            System.out.println("Subject [1" + i +"] : " + marksToShow[i] + "");
+        }
+
+        System.out.println(
+                    "Total Marks : " + this.totalMarks + "\n" +
+                    "Percentage : " + this.percentage + "\n" +
+                    "Result : " + this.result + "\n"
+        );
 
     }
 
