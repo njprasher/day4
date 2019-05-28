1cea6f7178bd044388612dadedf3da1e15e5d672
Neeraj Prasher <prasher.neeraj99@gmail.com>
2019-05-27 21:41:36 -0400

    [Neeraj] Date Added to Details.

--- src/main/java/com/neeraj/Day4StudentResultExample.java
+++ src/main/java/com/neeraj/Day4StudentResultExample.java
@@ -1,12 +1,29 @@
 package com.neeraj;
 
+import java.util.Date;
+
 public class Day4StudentResultExample
 {
     public static void main(String[] args)
     {
         StudentMain s1 = new StudentMain();
-        //s1.setStudentId(1);
-        //System.out.println(s1.toString());
 
+        s1.setStudentId(1);
+        s1.setFirstName("Neeraj");
+        s1.setLastName("Prasher");
+        s1.setGender("Male");
+        s1.setBirthDate(new Date(System.currentTimeMillis()));
+        float[] myMarks = { 77.0f, 88.0f, 79.0f, 94.0f, 91.0f };
+        s1.setMarks(myMarks);
+
+
+
+
+
+        s1.calculateTotal();
+        s1.calculatePercentage();
+        s1.calculateResult();
+
+        s1.displayStudentInformation();
     }
 }
